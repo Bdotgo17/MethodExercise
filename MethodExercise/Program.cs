dd2856a349f2084cf18f53530626cd26d0824579
Bdotgo17 <bdotgo@hotmail.com>
2022-09-14 14:18:50 -0500

    MethodExercise

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //exercise one
+
+            Console.WriteLine("What is your name ?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hi {name}. What is your favorite color ?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awsome color. What is your favorite animal ?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+        }
+    }
+}
+
